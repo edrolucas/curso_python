8ff3cbadc6af83de1eb07dec9c4ff333069d80ea
Pedroaraujos <plpatricio@Hotmail.com>
2024-12-02 20:29:34 -0300

    Add files via upload

---
+++ aula_4.py
@@ -0,0 +1,13 @@
+#Exercicio para ver se o numero inteiro é verdadeiro ou falso
+#True == par
+#False == impar
+
+numero = int(input("Digite o numero \n"))
+
+
+if numero % 2 == 0:
+    print("O", numero , 'é par')
+else:
+     print("O", numero , 'é impar')
+
+
