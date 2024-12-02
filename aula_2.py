3027de2ef28232aea566d25c8144a25d14b868d8
Pedroaraujos <plpatricio@Hotmail.com>
2024-12-02 19:41:04 -0300

    Add files via upload

---
+++ aula_2.py
@@ -0,0 +1,13 @@
+valor_compras = float(input('Digite o valor total das compras:'))
+desconto = float(input('Digite o valor do desconto:'))
+quantidade_itens = int(input('Digite a quantidade de itens que foram comprados:'))
+
+valordesconto = valor_compras * desconto
+valorfinal =  valor_compras - valordesconto
+
+custo_medio = valorfinal / quantidade_itens
+
+
+
+print( 'O valor do custo medio é : ' + str(custo_medio))
+print('O valor final das compras com desconto é : ' + str(valorfinal))
