97b53c3ba927e444143e0e3f1d70e4bb5fe0970c
Pedroaraujos <plpatricio@Hotmail.com>
2024-12-02 22:58:13 -0300

    Exercicio 4
    Alice é responsável por escrever um programa que verifica se um cupom de desconto pode ser utilizado. O programa recebe 3 valores e retorna True caso o cupom possa ser utilizado, ou False, caso contrário.
    
    Os três valores que o programa recebe são:
    
    Valor de compra (float)
    Valor do frete (float)
    Cliente é cadastrado no programa de fidelidade (string “s” (sim) ou “n” (não))
    O cupom tem a seguinte regra:
    
    Caso o valor da compra somado ao frete seja superior a R$ 100, ou o cliente seja cadastrado no programa de fidelidade, o cupom de desconto pode ser utilizado. Caso contrário, o cupom não pode ser utilizado.

--- aula_4.py
+++ aula_4.py
@@ -1,13 +1,21 @@
-#Exercicio para ver se o numero inteiro é verdadeiro ou falso
-#True == par
-#False == impar
+#Esse é o exercicio 4 do modulo 4
+#Usei o if de novo para facilitar minha vida
 
-numero = int(input("Digite o numero \n"))
+Valor_de_compra = float(input("Digite o valor da compra:\n"))
+Valor_do_Frete = float(input("Valor do frete: \n"))
+Desconto = float(input('Valor do desconto: \n'))
+Cadastro = str(input("Esse bct é cadastrado: \n"))
 
 
-if numero % 2 == 0:
-    print("O", numero , 'é par')
+Valor_total = Valor_de_compra + Valor_do_Frete
+Valor_Desconto = Valor_total * Desconto
+Valor_Final = Valor_total - Valor_Desconto
+
+if (Valor_total > 100 ) and (Cadastro == "S"):
+
+ print('Valor Final é de', Valor_Final)
+
 else:
-     print("O", numero , 'é impar')
-
-
+    print('Valor do Desconto não aplicado')
+    
+    
